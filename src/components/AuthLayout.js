21385a65837f4089a88491d06b1e080c02aa656d
Tarun <tarunbehera10@gmail.com>
2025-03-18 01:19:28 +0530

    committed on 18-03-25

---
+++ src/components/AuthLayout.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import LoginForm from "./LoginForm";
+import Welcome from "./Welcome";
+
+const AuthLayout = () => {
+  return (
+    <div className="app-container">
+      {/* Background circles */}
+      <div className="circle-blue-1"></div>
+      <div className="circle-blue-2"></div>
+      
+      <div className="main-layout">
+        {/* Welcome Section (Left Side) */}
+        <div className="welcome-section">
+          <Welcome />
+        </div>
+        {/* Login Form (Right Side) */}
+        <div className="login-section">
+          <LoginForm />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthLayout;
