490d5a3e5927d59fa0b73d16065f1eaaf54faebe
Tarun <tarunbehera10@gmail.com>
2025-03-26 23:52:09 +0530

    Updated on 26-3-25

--- App.js
+++ App.js
@@ -3,19 +3,28 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AuthLayout from "./src/components/AuthLayout";
 import Home from "./src/components/Home";
 import ProductDetails from "./src/components/ProductDetails";
+import ProductList from "./src/components/ProductList";
+import Cart from "./src/components/Cart";
+import CartProvider from "./src/components/CartContext";
+import Admin from "./src/components/Admin";
+import Order from "./src/components/Order";
 
 
 const App = () => {
   return (
+    <CartProvider>
     <Router>
       <Routes>
-        {/* element is component */}
         <Route path="/" element={<AuthLayout />} />
         <Route path="/login" element={<AuthLayout />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/products" element={<ProductDetails />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/orders" element={<Order />} />
       </Routes>
     </Router>
+    </CartProvider>
   );
 };
 
