2db7c0e846714dfb5d0529dab995431b273b2394
Tarun <tarunbehera10@gmail.com>
2025-03-28 23:25:13 +0530

    Updated on 28-3-25

---
+++ WoofNibblesBackEnd/woofnibbles/src/main/java/com/ex/controller/OrderController.java
@@ -0,0 +1,51 @@
+//package com.ex.controller;
+//
+//import com.ex.model.Order;
+//import com.ex.service.OrderService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/api/admin")
+//@CrossOrigin(origins = "http://localhost:3000")
+//public class OrderController {
+//    @Autowired
+//    private OrderService orderService;
+//
+//    @GetMapping("/orders")
+//    public List<Order> getAllOrders() {
+//        return orderService.getAllOrders();
+//    }
+//}
+
+
+
+package com.ex.controller;
+
+import com.ex.model.Order;
+import com.ex.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin")
+@CrossOrigin(origins = "http://localhost:3000")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping("/orders")
+    public List<Order> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+
+    @PostMapping("/orders")
+    public Order createOrder(@RequestBody Order order) {
+        return orderService.createOrder(order);
+    }
+}
