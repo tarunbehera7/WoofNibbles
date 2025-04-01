f01b26da8eea393524588d19db10393e0e3f4616
Tarun <tarunbehera10@gmail.com>
2025-04-02 01:13:00 +0530

    Updated on 2-4-25

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,23 +3,26 @@ import Navbar from './Navbar';
 import products from '../utils/mockData';
 import ProductCard from './ProductCard';
 import "../styles/ProductList.css";
+import Footer from './Footer';
 
 function ProductList() {
     
     const firstName = localStorage.getItem("firstName") || "User";
 
     return (
+        <>
+        <Navbar username={firstName} /> 
         <div className="page-container">
-            
-            <Navbar username={firstName} /> 
-
             <div className="product-list">
                 {products.map((product) => (
                     <ProductCard key={product.id} 
                         product={product} />
                 ))}
             </div>
+            
         </div>
+        <Footer/>
+        </>
     );
 }
 
