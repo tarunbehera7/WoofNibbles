b9af8839bc81bf6e7ea7580ed69e97e658e62d73
Tarun <tarunbehera10@gmail.com>
2025-04-17 01:33:18 +0530

    updated on 17-4-25

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -33,43 +33,12 @@ const LoginForm = () => {
 
 
   const handleSubmit = async (e) => {
+
     e.preventDefault();
     setError("");
     setSuccess("");
 
-
-    // ----Validation for Login----
-    if (activeTab === "login") {
-      if (!loginEmail.includes("@") || !loginEmail.includes(".")) {
-        setError("Please enter a valid email address");
-        return;
-      }
-      // if (loginPassword.length < 8) {
-      //   setError("Password must be at least 8 characters long");
-      //   return;
-      // }
-    }
-    // Validation for Signup
-    else {
-      if (!signupFirstName.match(/^[A-Za-z]+$/) || signupFirstName.length < 2) {
-        setError("First name must contain only letters and be at least 2 characters long");
-        return;
-      }
-      if (!signupLastName.match(/^[A-Za-z]+$/) || signupLastName.length < 2) {
-        setError("Last name must contain only letters and be at least 2 characters long");
-        return;
-      }
-      if (!signupEmail.includes("@") || !signupEmail.includes(".")) {
-        setError("Please enter a valid email address");
-        return;
-      }
-      if (signupPassword.length < 8 || !/\d/.test(signupPassword)) {
-        setError("Password must be at least 8 characters long and contain at least one number");
-        return;
-      }
-    }
-
-
+    
     try {
       if (activeTab === "login") {
         const response = await axios.post("http://localhost:8080/api/auth/login", {
